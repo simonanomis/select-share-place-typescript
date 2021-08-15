c797525a2bb4ccccf429072af07c99b398334881
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-08-15 14:21:19 +0200

    setting up the google api key

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # select-share-place-typescript
 Select &amp; Share a Place App (incl. Google Maps)
+
+This is a simple web app where we can enter an address,
+then we can look up this address and convert it into a pair of coordinates with help of Google API.
+After we can render this coordinates on a map with help of Google Maps JavaScript SDK.
+
+**Compile:**
+`tsc app.ts`
+With this it spits out the js file from ts file.
+
+**Start:**
+`npm install`
+`npm start`
+
+**Run**: tsc OR tsc -w
+
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-// Code goes here!
+import axios from "axios";
+
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+
+const GOOGLE_API_KEY = "AIzaSyCIaAc2c5M3VpbCH6PPq_guwy9lHuowXOs";
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    //send to Google API
+    axios
+        .get<GoogleGeocodingResponse>(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+                enteredAddress
+            )}&key=${GOOGLE_API_KEY}`
+        )
+        .then(response => {
+            if (response.data.status !== "OK") {
+                throw new Error("Could not fetch location!");
+            }
+            const coordinates = response.data.results[0].geometry.location;
+        })
+        .catch(err => {
+            alert(err.message);
+            console.log(err);
+        });
+}
+
+form.addEventListener('submit', searchAddressHandler);
